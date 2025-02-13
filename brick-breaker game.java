e2bc860c5ee60eb00f95dbfba7fa43807eede03e
Yuvr4j-prog <yuuvrrajj@gmail.com>
2025-02-13 20:00:08 +0530

    Update brick-breaker game.java

--- brick-breaker game.java
+++ brick-breaker game.java
@@ -1,1 +1,11 @@
 
+public class java1 { 
+
+    public static void main(String[] args){
+    int num = 0;
+    for(int i=0;i <= 4;i++)
+    { for(int j=1;j<=i;j++)
+        {System.out.print(num  + " ");
+    num++;} System.out.println();
+    }
+}  }
